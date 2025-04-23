719938ce0834fdf7f188d549947f74ee242ec6a5
mohammad <mh.hashemi@netafraz.com>
2025-04-23 15:58:58 +0330

    📦 FEAT: added new types

---
+++ src/types/components.ts
@@ -0,0 +1,23 @@
+import type { Component } from 'vue';
+
+export interface StatItem {
+  label: string;
+  value: string;
+  changePercentage: number;
+  changeText: string;
+  icon: Component;
+}
+
+export interface Activity {
+  icon: Component;
+  title: string;
+  time: string;
+  description: string;
+}
+
+export interface Product {
+  icon: Component;
+  name: string;
+  category: string;
+  price: string;
+}
