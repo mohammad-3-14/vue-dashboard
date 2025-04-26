528c061f48874417a2fad79b2218d2c611f4c492
mohammad <mh.hashemi@netafraz.com>
2025-04-26 15:48:39 +0330

    📦 FEAT: Add i18n support and translation files

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import './assets/main.css';
-import Router from './router';
-
 import { createApp } from 'vue';
-import App from './App.vue';
+import Router from './router';
+import './assets/main.css';
+import i18n from './plugins/i18n';
+import App from '@/App.vue';
 
 const app = createApp(App);
 app.use(Router);
+app.use(i18n);
 app.mount('#app');
