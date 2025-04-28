c684db8be4ce6eab8f2bb1619e60f83f7fe33c0c
mohammad <mh.hashemi@netafraz.com>
2025-04-28 11:48:51 +0330

    📦 FEAT:  set html lang attribute based on i18n locale

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts">
 import DefaultLayout from '@/layouts/default.vue';
+import { onMounted } from 'vue';
+import { useI18n } from 'vue-i18n';
+
+const { locale } = useI18n();
+
+onMounted(() => {
+  if (locale.value === 'fa') {
+    document.documentElement.lang = 'fa';
+  }
+});
 </script>
 
 <template>
