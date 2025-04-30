61d360aca889b5be2a790c25af8a3f4f5adc9a66
mohammad <mh.hashemi@netafraz.com>
2025-04-30 15:38:44 +0330

    🎨 STYLE:  update button colors to match core theme

--- src/components/shared/button.vue
+++ src/components/shared/button.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="p-2 h-9 bg-teal-800 hover:bg-teal-700 rounded-lg cursor-pointer"
+    class="p-2 h-9 bg-core-900 hover:bg-core-800 rounded-lg cursor-pointer"
   >
     <slot />
   </button>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -15,10 +15,10 @@ const isSidebarCollapsed = ref<boolean>(true);
 </script>
 
 <template>
-  <div class="min-h-screen bg-teal-900 w-full">
-    <div class="text-gray-200 container mx-auto px-5">
+  <div class="min-h-screen bg-core-900 w-full">
+    <div class="text-text container mx-auto px-5">
       <Header @toggleSidebar="toggleSidebar" @toggleDrawer="toggleDrawer" />
-      <div class="flex md:gap-5 mt-5">
+      <div class="flex md:gap-5 mt-7">
         <!-- Sidebar Fixed -->
         <Sidebar
           :isSidebarCollapsed="isSidebarCollapsed"
